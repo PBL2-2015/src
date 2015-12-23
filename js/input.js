e2da290c4fecefbdadbf6e06ca3900985b33b0e7
古賀祥造 <koga@f.ait.kyushu-u.ac.jp>
2015-12-23 10:00:30 +0900

    [add]div要素をcanvasタグの上に配置させる機能
    - canvasタグの中にいれるのではなく
    <canvas></canvas>
    <div class="fusen">テキスト</div>
    として，座標を調整して重なっているように見せている
    
    参考URL
    （http://www.marguerite.jp/Nihongo/WWW/DHTML/Canvas/TextOntoCanvas.html######PUT_DOM_NODES_OVER_THE_CAMVAS）

--- js/input.js
+++ js/input.js
@@ -1,22 +1,46 @@
 
+
+// e=document.createElement('div');
+// e.setAttribute('id', 'HORIKITA_MAKI_TXT'); /* あとで除去するのに利用する"目印" */
+// e.style.position='absolute';
+// e.style.top='120px';
+// e.style.left='60px';
+// e.style.width='200px';
+// e.style.height='20px';
+// e.style.background='transparent';
+// e.style.color='#fff';
+// e.style.textAlign='center';
+// e.style.font='normal normal 20px/32px sans-serif';
+// parentOfTheCanvas.appendChild(e);
+// e.innerHTML='堀北真希ちゃん';
+
+// ------
+
 var i=1; //idインクリメント
 
 function fusen_display(){
 
 	var input = window.prompt("アイデアを入力してください");
 
-	var canvas = document.getElementById("canvas")
+	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
 	
-	ctx.save();  // おまじない
+	// ctx.save();  // おまじない
 
-	ctx.font = "40px meiryo";
-	ctx.fillText(input, 50, 30);
+	// ctx.font = "40px meiryo";
+	// ctx.fillText(input, 50, 30);
 
-	// $('canvas').append('<div id='+ i + ' class="fusen">'+ input + '</div>');
+	$('#canvas-wrap').append('<div id='+ i + ' class="fusen" draggable="true">'+ input + '</div>');
 
-	// i = i + 1;
+	// var element = document.createElement('div');
+	// element.id = i;
+	// element.className = 'fusen';
+	// element.innerHTML = input;
 
-   ctx.restore();  // おまじない
+	// parent_of_the_canvas.appendChild(element);
+
+	i = i + 1;
+
+   // ctx.restore();  // おまじない
 
 }
