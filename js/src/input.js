6c187dc14d9d0aefa421d17d0afc4c7e36ca6105
古賀祥造 <koga@f.ait.kyushu-u.ac.jp>
2016-01-18 17:56:39 +0900

    [add]1クリックでcanvasをキャプチャしてダウンロードする機能を実装

--- js/src/input.js
+++ js/src/input.js
@@ -136,20 +136,26 @@ $(document).keydown(function(e){
 });
 
 // スクリーンショット機能
-function screenshot(selector) {
-    var element = $(selector)[0];
-    html2canvas(element, {onrendered:function(canvas) {
-        var imgData = canvas.toDataURL();
-        // $('#screen_image')[0].src = imgData;
-        $('#download')[0] = 'sample.png' 
-        $('#download')[0].href = imgData;
-        $('#download')[0].innerHTML = "ダウンロード";
-    }});
-}
 
-// function erase_screenshot() {
-//     $('#screen_image')[0].src = "";
-//     $('#download')[0].href = "#";
-//     $('#download')[0].innerHTML = "";
-// }
+$(document).on('click','#capture_btn', function(){
+
+	var element = $('#canvas-wrap')[0];
+
+    html2canvas(element, {onrendered:function(canvas) {
+
+     	var imgData = canvas.toDataURL("image/png");
+
+	// ダウンロードリンクの生成
+      	var downloadLink = document.createElement("a");
+     	downloadLink.download = 'sample.png';
+     	downloadLink.href = imgData;
+
+     	downloadLink.click();
+
+    }});
+
+});
+
+
+
 
